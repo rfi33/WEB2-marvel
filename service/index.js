d6a5ebd14e3b660f53967e00c6f9dbee41b926a8
Miranto <hei.finoana.5@gmail.com>
2025-08-15 18:08:12 +0300

    feat: ajout du fonctionnement sauvegarde

--- service/index.js
+++ service/index.js
@@ -1,15 +1,10 @@
 import express from "express"
 import router from "./routes/post.js"
-import db from './models/characters.json' with { type: "json"};
 
 const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.db = db;
-  next();
-});
 
 const PORT = 4000;
 
--- service/routes/post.js
+++ service/routes/post.js
@@ -1,7 +1,35 @@
 import express from "express";
-import charactersData from "../models/characters.json" with { type: "json"};
+import path from "path";
+import fs from "fs/promises";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const jsonFilePath = path.join(__dirname, "../models/characters.json");
 
 const router = express.Router();
+
+const readCharactersFile = async () => {
+    try {
+        const data = await fs.readFile(jsonFilePath,'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error("Read file error",error);
+        return {characters:[]};
+    }
+};
+
+const writeCharactersFile = async (data) => {
+    try{
+        await fs.writeFile(jsonFilePath,JSON.stringify(data,null,2),'utf8');
+    }catch(error){
+        console.error('Write file error',error);
+        return false;
+
+    }
+};
+
+const charactersData =await readCharactersFile();
 
 router.get("/",(req,res) => {
      res.status(200).json(charactersData);
@@ -9,7 +37,6 @@ router.get("/",(req,res) => {
 
 router.post("/create", async (req,res) => {
     const character = req.body;
-
     try{
         if(!character.name || !character.realName || !character.universe){
             return res.status(400).json({
@@ -21,9 +48,10 @@ router.post("/create", async (req,res) => {
         const maxId = charactersData.characters.reduce((max, char) => Math.max(max, char.id), 0);
         character.id = maxId + 1;
         }
+        
+       charactersData.characters.push(character);
+       await writeCharactersFile(charactersData);
 
-       charactersData.characters.push(character)
-     
      res.status(201).json({
         message : "Character created successfully",
         character     
@@ -65,7 +93,7 @@ router.put("/edit/:id", async (req, res) => {
         const idCharacter = parseInt(req.params.id);
         const updatedData = req.body;
 
-        if (isNaN(idCharacter) || idCharacter <= 0 || charactersData.characters.find(char => char.id === idCharacter)) {
+        if (isNaN(idCharacter) || idCharacter <= 0 || charactersData.characters.findIndex(char => char.id === idCharacter) > 0) {
             return res.status(400).json({
                 error: "ID de caractère invalide"
             });
@@ -82,6 +110,7 @@ router.put("/edit/:id", async (req, res) => {
             message: "Caractère modifié avec succès",
             character: charactersData.characters[characterIndex]
         });
+               await writeCharactersFile(charactersData);
 
     } catch (error) {
         res.status(500).json({
@@ -91,20 +120,27 @@ router.put("/edit/:id", async (req, res) => {
 });
 
 
-router.delete("/delete/:id", async (req,res)=>{
+router.delete("/delete/:id", async (req,res)=>
+    {
     try{
         const idCharacter = parseInt(req.params.id);
-        if(isNaN(idCharacter) || idCharacter <=0){
+        if(isNaN(idCharacter) || idCharacter <=0 ){
             res.status(400).json({
                 error:"ID not found"
             })
         }
-        const characterIndex = charactersData.characters.findIndex(char => char.id == idCharacter);
 
-      const deletedCharacter = charactersData.characters.splice(characterIndex, 1)[0];
+        const characterIndex = charactersData.characters.findIndex(char => char.id === idCharacter);
+if (characterIndex > 0 ) {
+    const deletedCharacter = charactersData.characters.splice(characterIndex, 1);
+    res.status(200).json({ message: "Character deleted", character: deletedCharacter });
+} else {
+    res.status(400).json({ error: "ID not found" });
+}
+            await writeCharactersFile(charactersData);
 
         res.status(200).json({
-            message:"Character :"+idCharacter+"delete)",
+            message:"Character :"+idCharacter+"deleted",
             character: deletedCharacter
         })
 

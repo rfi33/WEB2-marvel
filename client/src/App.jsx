23cbd812fa5753aa623e07dd3d1e1739d97d23ad
Miranto <hei.finoana.5@gmail.com>
2025-08-16 11:50:05 +0300

    feat: formulaire creation d heros et fonction delete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,12 @@ function App() {
    const [characters,setChacters] = useState([]);
    const [isLoading,setLoading] = useState(false);
    const [error, setError] = useState(null);
+      const [newCharacter, setNewCharacter] = useState({
+     name: '',
+     realName: '',
+     universe: ''
+   });
+    const [showAddForm, setShowAddForm] = useState(false);
 
     const fetchPost = async ()=>{
       setLoading(true); 
@@ -32,9 +38,41 @@ function App() {
       }finally{
           setLoading(false);
       }
-      
     };
 
+       const addCharacter = async () => {
+     if (!newCharacter.name || !newCharacter.realName) return;
+
+     try {
+       const response = await fetch(`${BASE_URL}/create`, {
+         method: 'POST',
+         headers: {
+           'Content-Type': 'application/json'
+         },
+         body: JSON.stringify(newCharacter)
+       });
+
+       if (response.ok) {
+         setNewCharacter({ name: '', realName: '', universe: '' });
+         setShowAddForm(false);
+         fetchPost();
+       }
+     } catch (error) {
+       console.error('Error adding character:', error);
+     }
+   };
+
+    const deleteCharacter = async (id) => {
+      try {
+        const response = await fetch(`${BASE_URL}/delete/${id}`, { method: 'DELETE' });
+
+        if(response.ok){
+          fetchPost();
+        }
+      } catch (error) {
+        console.error('Error deleting character:',error)
+      }      
+    };
 
       useEffect(() => {
      fetchPost();
@@ -64,7 +102,48 @@ function App() {
     <>
     <div>
       <h1 className='text-center text-5xl text-white p-4 bg-red-600'>Marvel HEROS</h1>
-  
+   {showAddForm && (
+          <div className="bg-gray-100 p-4 border-b">
+            <h3 className="text-xl font-bold  text-center text-gray-800">Add new heros</h3>
+            <div className="flex justify-center pt-4 gap-3 items-center">
+              <input
+                type="text"
+                placeholder="Hero's Name"
+                value={newCharacter.name}
+                onChange={(e) => setNewCharacter({newCharacter, name: e.target.value})}
+                className="px-3 py-2 border border-gray-300 rounded"
+              />
+              <input
+                type="text"
+                placeholder="Real name"
+                value={newCharacter.realName}
+                onChange={(e) => setNewCharacter({newCharacter, realName: e.target.value})}
+                className="px-3 py-2 border border-gray-300 rounded"
+              />
+              <input
+                type="text"
+                placeholder="Universe"
+                value={newCharacter.universe}
+                onChange={(e) => setNewCharacter({newCharacter, universe: e.target.value})}
+                className="px-3 py-2 border border-gray-300 rounded"
+              />
+              <div className="flex gap-2">
+                <button
+                  onClick={addCharacter}
+                  className="px-4 py-2 bg-green-600 text-white hover:opacity-80 rounded transition-opacity duration-150"
+                >
+                  Add
+                </button>
+                <button
+                  onClick={() => setShowAddForm(false)}
+                  className="px-4 py-2 bg-amber-500 text-white rounded hover:opacity-80 transition-opacity duration-150"
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
   
   <table className="table-auto border-collapse border border-gray-300 w-full">
   <thead>
@@ -73,7 +152,9 @@ function App() {
       <th className="border border-gray-300 px-4 py-2 text-left">Nom</th>
       <th className="border border-gray-300 px-4 py-2 text-left">Nom réel</th>
       <th className="border border-gray-300 px-4 py-2 text-left">Univers</th>
-      <th className="border border-gray-300 px-4 py-2 "><button className='bg-green-600 hover:scale-115 transition-transform duration-300 px-5 py-2 rounded-3xl text-white'>ADD<span className='text-xl'>+</span></button></th>
+      <th className="border border-gray-300 px-4 py-2 "><button 
+      onClick={()=> setShowAddForm(true)}
+      className='bg-green-600 hover:scale-115 transition-transform duration-300 px-5 py-2 rounded-3xl text-white'>ADD<span className='text-xl'>+</span></button></th>
     </tr>
   </thead>
   <tbody>
@@ -87,7 +168,7 @@ function App() {
         <button className='bg-blue-900 text-white p-2 border transition-opacity hover:opacity-80 duration-300'>
           Edit
         </button>
-        <button className='bg-red-600 text-white p-2 border transition-opacity hover:opacity-80 duration-300'>
+        <button onClick={()=>deleteCharacter(character.id)} className='bg-red-600 text-white p-2 border transition-opacity hover:opacity-80 duration-300'>
          Delete
         </button> 
         </div>
